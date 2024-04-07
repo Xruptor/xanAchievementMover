4df1e6f748c58273cc1eac761b224cb7e3f64ffd
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-04-07 18:33:51 +0800

    Create zhTW.lua

---
+++ locale/zhTW.lua
@@ -0,0 +1,7 @@
+local ADDON_NAME, addon = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "zhTW")
+if not L then return end
+
+L.DragFrameInfo = "xanAchievementMover \n\n移動到位後右鍵點擊。"
+L.AnchorText = "切換窗口錨點"
