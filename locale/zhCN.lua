9c3a2974e786daf097dfe67cbb3822df82c0b5ea
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-04-07 18:33:13 +0800

    Create zhCN.lua

---
+++ locale/zhCN.lua
@@ -0,0 +1,7 @@
+local ADDON_NAME, addon = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "zhCN")
+if not L then return end
+
+L.DragFrameInfo = "xanAchievementMover \n\n移动到位后右键点击窗口。"
+L.AnchorText = "切换窗口锚点"
