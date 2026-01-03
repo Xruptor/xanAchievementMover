e8ce4fe832a2f4fe003e9acd91012dccc8db1d0e
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-01-03 07:29:24 +0300

    Create ruRU.lua

---
+++ locale/ruRU.lua
@@ -0,0 +1,7 @@
+local ADDON_NAME, addon = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "ruRU")
+if not L then return end
+-- Translator ZamestoTV
+L.DragFrameInfo = "xanAchievementMover \n\nПКМ, когда закончите перетаскивание"
+L.AnchorText = "Переключить якорь фрейма"
